3de4d0984d478f4126adc236994a605aba8bb94a
Joseph Petersen <josephp90@gmail.com>
2022-06-03 04:27:20 +0200

    support action input for release draft body

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 set -euxo pipefail
 if [ $GITHUB_EVENT_NAME = check_run ]
 then
-    RESULT=$(gh api /repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .body' | egrep "$INTERESTING_CATEGORIES" || echo 'failed')
+    if [ -z "$RELEASE_DRAFT_BODY" ]; then
+        RESULT=$(gh api /repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .body' | egrep "$INTERESTING_CATEGORIES" || echo 'failed')
+    else
+        RESULT=$(echo "$RELEASE_DRAFT_BODY" | egrep "$INTERESTING_CATEGORIES" || echo 'failed')
+    fi
     if [[ $RESULT != 'failed' ]]; then
       echo "::set-output name=interesting::true"
     else
