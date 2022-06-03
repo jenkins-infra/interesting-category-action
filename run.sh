e85a17a27f5ba38c2638ca986bce8b1baf792ead
Joseph Petersen <josephp90@gmail.com>
2022-06-03 20:03:40 +0200

    Update run.sh
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ set -euxo pipefail
 if [ $GITHUB_EVENT_NAME = check_run ]
 then
     if [ -z "$RELEASE_DRAFT_BODY" ]; then
-        RESULT=$(gh api /repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .body' | egrep "$INTERESTING_CATEGORIES" || echo 'failed')
-    else
-        RESULT=$(echo "$RELEASE_DRAFT_BODY" | egrep "$INTERESTING_CATEGORIES" || echo 'failed')
+        RELEASE_DRAFT_BODY="$(gh api /repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .body')"
     fi
+
+    RESULT=$(echo "$RELEASE_DRAFT_BODY" | egrep "$INTERESTING_CATEGORIES" || echo 'failed')
     if [[ $RESULT != 'failed' ]]; then
       echo "::set-output name=interesting::true"
     else
