e19d1eea56fad7fc79edf9eb965d20948f6b0003
Joseph Petersen <josephp90@gmail.com>
2022-06-13 21:41:07 +0200

    simplify if statement

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 set -euxo pipefail
 if [ $GITHUB_EVENT_NAME = check_run ]
 then
-    if [ -z "$RELEASE_DRAFT_BODY" ]; then
+    if [ -z "$RELEASE_DRAFT_BODY" ]
+    then
         RELEASE_DRAFT_BODY="$(gh api /repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .body')"
     fi
 
-    RESULT=$(echo "$RELEASE_DRAFT_BODY" | egrep "$INTERESTING_CATEGORIES" || echo 'failed')
-    if [[ $RESULT != 'failed' ]]; then
+    if echo "$RELEASE_DRAFT_BODY" | egrep "$INTERESTING_CATEGORIES"
+    then
       echo "::set-output name=interesting::true"
     else
       echo "::set-output name=interesting::false"
